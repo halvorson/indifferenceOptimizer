0c96399f0712d359ad86871a96ad74f9648e5bcf
halvorson <michael.t.halvorson@gmail.com>
2017-08-26 00:44:01 -0700

    trying to tease out env variables

--- models/index.js
+++ models/index.js
@@ -33,4 +33,15 @@ Object.keys(db).forEach(function(modelName) {
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
+// db.preference.belongsTo(db.user);
+// db.user.hasMany(db.preference);
+// db.preference.belongsTo(db.campaign);
+// db.campaign.hasMany(db.preference);
+// db.campaign.hasMany(db.timeslot);
+// db.timeslot.belongsTo(db.campaign);
+// db.preference.belongsTo(db.timeslot);
+// db.timeslot.hasMany(db.preference);
+
+console.log(process.env[config.use_env_variable]);
+
 module.exports = db;
